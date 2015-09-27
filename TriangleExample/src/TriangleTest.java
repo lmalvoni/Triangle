47e64fc7e985317ccb57d832a22630aecf28340a
luciamalvoni <lmalvoni@udel.edu>
2015-09-27 19:06:37 -0400

    initial

---
+++ TriangleExample/src/TriangleTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleTest {
+	Triangle Triangle = new Triangle ();
+
+	@Test
+	public void main (String[] Args) {
+		Triangle Triangle = new Triangle(1,2,1);
+	}
+
+}
